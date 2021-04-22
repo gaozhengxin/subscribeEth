9061afcecb0adcd272c91885c10553985c11dc19
gaozhengxin <zhengxingao@live.cn>
2021-04-22 11:52:03 +0800

    code refactor

--- subscribe.go
+++ subscribe.go
@@ -4,38 +4,41 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"net/http"
-	"time"
-	"strings"
 	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 	//"math/big"
 
 	"github.com/BurntSushi/toml"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 type Config struct {
-	Endpoint string
-	Server string
-	ContractAddresses []string
+	Endpoint      string
+	Server        string
+	SwapoutTokens []string
+	SwapinTokens  []string
 }
 
 var s struct {
-    FOO struct {
-        Usernames_Passwords map[string]string
-    }
+	FOO struct {
+		Usernames_Passwords map[string]string
+	}
 }
 
 var configFile string
 
-var SwapoutTopic common.Hash = common.HexToHash("0x6b616089d04950dc06c45c6dd787d657980543f89651aec47924752c7d16c888")
-var BTCSwapoutTopic common.Hash = common.HexToHash("0x9c92ad817e5474d30a4378deface765150479363a897b0590fbb12ae9d89396b")
-var ERC20TransferTopic common.Hash = common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+var (
+	SwapoutTopic       common.Hash = common.HexToHash("0x6b616089d04950dc06c45c6dd787d657980543f89651aec47924752c7d16c888")
+	BTCSwapoutTopic    common.Hash = common.HexToHash("0x9c92ad817e5474d30a4378deface765150479363a897b0590fbb12ae9d89396b")
+	ERC20TransferTopic common.Hash = common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef")
+)
 
 func init() {
 	log.Root().SetHandler(log.StdoutHandler)
@@ -56,33 +59,14 @@ func main() {
 
 	config := LoadConfig()
 
-	// 合约地址
-	var addressesMap = make(map[common.Address]string)
-	var serverMap = make(map[common.Address]string)
-	var addresses = make([]common.Address, 0)
+	go StartSubscribeHeader(config)
+	go StartSubscribeSwapout(config)
 
-	var endpoint string =  config.Endpoint
+	select {}
+}
 
-	topics := make([][]common.Hash, 0)
-	//topics = append(topics, []common.Hash{SwapoutTopic}) // SwapoutTopic or BTCSwapoutTopic
-	topics = append(topics, []common.Hash{SwapoutTopic, BTCSwapoutTopic}) // SwapoutTopic or BTCSwapoutTopic
-
-	for _, item := range config.ContractAddresses {
-		pairID := strings.Split(item, ",")[0]
-		addr := common.HexToAddress(strings.Split(item, ",")[1])
-		server := strings.Split(item, ",")[2]
-
-		addressesMap[addr] = pairID
-		serverMap[addr] = server
-		addresses = append(addresses, addr)
-	}
-
-	fq := ethereum.FilterQuery{
-		Addresses: addresses,
-		Topics:    topics,
-	}
-
-	log.Info("fq", "fq", fq)
+func StartSubscribeHeader(config *Config) {
+	var endpoint string = config.Endpoint
 
 	ctx := context.Background()
 	client, err := ethclient.DialContext(ctx, endpoint)
@@ -90,56 +74,85 @@ func main() {
 		panic(err)
 	}
 
-	ch := make(chan types.Log, 128)
-	defer close(ch)
-
-	sub := LoopSubscribe(client, ctx, fq, ch)
-	defer sub.Unsubscribe()
-
-	go func() {
-		for {
-			select {
-			case msg := <-ch:
-				log.Info("Find event", "event", msg)
-				txhash := msg.TxHash.String()
-				pairID := addressesMap[msg.Address]
-				fmt.Printf("txhash: %v, pairID: %v\n", txhash, pairID)
-				server := serverMap[msg.Address]
-				swaperr := DoSwapout(txhash, pairID, server)
-				if swaperr != nil {
-					log.Warn("Do swapout error", "error", swaperr)
-				}
-			case err := <-sub.Err():
-				log.Info("Subscribe error", "error", err)
-				sub.Unsubscribe()
-				sub = LoopSubscribe(client, ctx, fq, ch)
-			}
-		}
-	} ()
-
 	headCh := make(chan *types.Header, 128)
 	defer close(headCh)
 
-	subHead := LoopSubscribeHead(client, ctx, headCh)
-	defer subHead.Unsubscribe()
+	sub := LoopSubscribeHead(client, ctx, headCh)
+	defer sub.Unsubscribe()
 
-	go func() {
-		for {
-			select {
-			case msg := <-headCh:
-				log.Trace("Get new header", "head", msg.Number)
-			case err := <-subHead.Err():
-				log.Info("Subscribe error", "error", err)
-				sub.Unsubscribe()
-				sub = LoopSubscribe(client, ctx, fq, ch)
-			}
+	for {
+		select {
+		case msg := <-headCh:
+			log.Trace("Get new header", "head", msg.Number)
+		case err := <-sub.Err():
+			log.Info("Subscribe error", "error", err)
+			sub.Unsubscribe()
+			sub = LoopSubscribeHead(client, ctx, headCh)
 		}
-	} ()
-
-	select{}
+	}
 }
 
-func LoopSubscribeHead(client *ethclient.Client, ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription) {
+func StartSubscribeSwapout(config *Config) {
+	var endpoint string = config.Endpoint
+
+	ctx := context.Background()
+	client, err := ethclient.DialContext(ctx, endpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	var dstAddressesMap = make(map[common.Address]string)
+	var dstServerMap = make(map[common.Address]string)
+	var dstAddresses = make([]common.Address, 0)
+
+	topics := make([][]common.Hash, 0)
+	topics = append(topics, []common.Hash{SwapoutTopic, BTCSwapoutTopic}) // SwapoutTopic or BTCSwapoutTopic
+
+	for _, item := range config.SwapoutTokens {
+		pairID := strings.Split(item, ",")[0]
+		addr := common.HexToAddress(strings.Split(item, ",")[1])
+		server := strings.Split(item, ",")[2]
+
+		dstAddressesMap[addr] = pairID
+		dstServerMap[addr] = server
+		dstAddresses = append(dstAddresses, addr)
+	}
+
+	swapoutfq := ethereum.FilterQuery{
+		Addresses: dstAddresses,
+		Topics:    topics,
+	}
+
+	log.Info("swapout fq", "swapoutfq", swapoutfq)
+
+	ch := make(chan types.Log, 128)
+	defer close(ch)
+
+	sub := LoopSubscribe(client, ctx, swapoutfq, ch)
+	defer sub.Unsubscribe()
+
+	// subscribe swapout
+	for {
+		select {
+		case msg := <-ch:
+			log.Info("Find event", "event", msg)
+			txhash := msg.TxHash.String()
+			pairID := dstAddressesMap[msg.Address]
+			fmt.Printf("txhash: %v, pairID: %v\n", txhash, pairID)
+			server := dstServerMap[msg.Address]
+			swaperr := DoSwapout(txhash, pairID, server)
+			if swaperr != nil {
+				log.Warn("Do swapout error", "error", swaperr)
+			}
+		case err := <-sub.Err():
+			log.Info("Subscribe error", "error", err)
+			sub.Unsubscribe()
+			sub = LoopSubscribe(client, ctx, swapoutfq, ch)
+		}
+	}
+}
+
+func LoopSubscribeHead(client *ethclient.Client, ctx context.Context, ch chan<- *types.Header) ethereum.Subscription {
 	for {
 		sub, err := client.SubscribeNewHead(ctx, ch)
 		if err == nil {
@@ -150,7 +163,7 @@ func LoopSubscribeHead(client *ethclient.Client, ctx context.Context, ch chan<- 
 	}
 }
 
-func LoopSubscribe(client *ethclient.Client, ctx context.Context, fq ethereum.FilterQuery, ch chan types.Log) (ethereum.Subscription) {
+func LoopSubscribe(client *ethclient.Client, ctx context.Context, fq ethereum.FilterQuery, ch chan types.Log) ethereum.Subscription {
 	for {
 		sub, err := client.SubscribeFilterLogs(ctx, fq, ch)
 		if err == nil {
