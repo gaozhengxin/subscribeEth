e0d536efd8b8a5564a50c6b0f2422e27c2a9c615
gaozhengxin <zhengxingao@live.cn>
2021-04-22 12:23:59 +0800

    add subscribe swapin

--- subscribe.go
+++ subscribe.go
@@ -61,6 +61,7 @@ func main() {
 
 	go StartSubscribeHeader(config)
 	go StartSubscribeSwapout(config)
+	go StartSubscribeSwapin(config)
 
 	select {}
 }
@@ -101,9 +102,9 @@ func StartSubscribeSwapout(config *Config) {
 		panic(err)
 	}
 
-	var dstAddressesMap = make(map[common.Address]string)
-	var dstServerMap = make(map[common.Address]string)
-	var dstAddresses = make([]common.Address, 0)
+	var addreeePairIDMap = make(map[common.Address]string)
+	var serverMap = make(map[common.Address]string)
+	var tokenAddresses = make([]common.Address, 0)
 
 	topics := make([][]common.Hash, 0)
 	topics = append(topics, []common.Hash{SwapoutTopic, BTCSwapoutTopic}) // SwapoutTopic or BTCSwapoutTopic
@@ -113,13 +114,13 @@ func StartSubscribeSwapout(config *Config) {
 		addr := common.HexToAddress(strings.Split(item, ",")[1])
 		server := strings.Split(item, ",")[2]
 
-		dstAddressesMap[addr] = pairID
-		dstServerMap[addr] = server
-		dstAddresses = append(dstAddresses, addr)
+		addreeePairIDMap[addr] = pairID
+		serverMap[addr] = server
+		tokenAddresses = append(tokenAddresses, addr)
 	}
 
 	swapoutfq := ethereum.FilterQuery{
-		Addresses: dstAddresses,
+		Addresses: tokenAddresses,
 		Topics:    topics,
 	}
 
@@ -137,9 +138,9 @@ func StartSubscribeSwapout(config *Config) {
 		case msg := <-ch:
 			log.Info("Find event", "event", msg)
 			txhash := msg.TxHash.String()
-			pairID := dstAddressesMap[msg.Address]
+			pairID := addreeePairIDMap[msg.Address]
 			fmt.Printf("txhash: %v, pairID: %v\n", txhash, pairID)
-			server := dstServerMap[msg.Address]
+			server := serverMap[msg.Address]
 			swaperr := DoSwapout(txhash, pairID, server)
 			if swaperr != nil {
 				log.Warn("Do swapout error", "error", swaperr)
@@ -149,6 +150,82 @@ func StartSubscribeSwapout(config *Config) {
 			sub.Unsubscribe()
 			sub = LoopSubscribe(client, ctx, swapoutfq, ch)
 		}
+	}
+}
+
+func StartSubscribeSwapin(config *Config) {
+	var endpoint string = config.Endpoint
+
+	ctx := context.Background()
+	client, err := ethclient.DialContext(ctx, endpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	var addreeePairIDMap = make(map[common.Address]string)
+	var serverMap = make(map[common.Address]string)
+	var depositAddressMap = make(map[common.Address][]common.Address)
+	var ETHDepositAddress = common.Address{}
+
+	for _, item := range config.SwapinTokens {
+		pairID := strings.Split(item, ",")[0]
+		tokenAddr := common.HexToAddress(strings.Split(item, ",")[1])
+		depositAddr := common.HexToAddress(strings.Split(item, ",")[2])
+		server := strings.Split(item, ",")[3]
+	
+		if tokenAddr == (common.Address{}) {
+			ETHDepositAddress = depositAddr
+		}
+
+		addreeePairIDMap[tokenAddr] = pairID
+		serverMap[tokenAddr] = server
+		if depositAddressMap[depositAddr] == nil {
+			depositAddressMap[depositAddr] = make([]common.Address, 1)
+		}
+		depositAddressMap[depositAddr] = append(depositAddressMap[depositAddr], tokenAddr)
+	}
+
+	// subscribe ERC20 swapin
+	for depositAddr, tokens := range depositAddressMap {
+		topics := make([][]common.Hash, 0)
+		if depositAddr != ETHDepositAddress {
+			topics = append(topics, []common.Hash{ERC20TransferTopic})       // Log [0] is ERC20 transfer
+			topics = append(topics, []common.Hash{})                         // Log [1] is arbitrary
+			topics = append(topics, []common.Hash{depositAddr.Hash()}) // Log [2] is deposit address
+		}
+
+		fq := ethereum.FilterQuery{
+			Addresses: tokens,
+			Topics:    topics,
+		}
+		log.Info("swapin fq", "depositAddr", fq)
+
+		go func() {
+			ch := make(chan types.Log, 128)
+			defer close(ch)
+
+			sub := LoopSubscribe(client, ctx, fq, ch)
+			defer sub.Unsubscribe()
+
+			for {
+				select {
+				case msg := <-ch:
+					log.Info("Find event", "event", msg)
+					txhash := msg.TxHash.String()
+					pairID := addreeePairIDMap[msg.Address]
+					fmt.Printf("txhash: %v, pairID: %v\n", txhash, pairID)
+					server := serverMap[msg.Address]
+					swaperr := DoSwapin(txhash, pairID, server)
+					if swaperr != nil {
+						log.Warn("Do swapout error", "error", swaperr)
+					}
+				case err := <-sub.Err():
+					log.Info("Subscribe error", "error", err)
+					sub.Unsubscribe()
+					sub = LoopSubscribe(client, ctx, fq, ch)
+				}
+			}
+		}()
 	}
 }
 
@@ -178,6 +255,27 @@ func LoopSubscribe(client *ethclient.Client, ctx context.Context, fq ethereum.Fi
 func DoSwapout(txid, pairID string, server string) error {
 	client := &http.Client{}
 	var data = strings.NewReader(fmt.Sprintf(`{"jsonrpc":"2.0","method":"swap.Swapout","params":[{"txid":"%v","pairid":"%v"}],"id":1}`, txid, pairID))
+	req, err := http.NewRequest("POST", server, data)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Info("Call swap server", "response", fmt.Sprintf("%s", bodyText))
+	return nil
+}
+
+func DoSwapin(txid, pairID string, server string) error {
+	client := &http.Client{}
+	var data = strings.NewReader(fmt.Sprintf(`{"jsonrpc":"2.0","method":"swap.Swapin","params":[{"txid":"%v","pairid":"%v"}],"id":2}`, txid, pairID))
 	req, err := http.NewRequest("POST", server, data)
 	if err != nil {
 		return err
